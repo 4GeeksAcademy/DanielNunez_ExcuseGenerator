043aad0fa09533bcb0e80332931714c5655e0a96
DanielNunez04 <130614771+DanielNunez04@users.noreply.github.com>
2023-06-13 00:26:15 +0000

    Daniel_Excuse_Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function onload() {
+  let whoRandom = who[Math.floor(Math.random() * (who.length - 1) + 1)];
+  let actionRandom =
+    action[Math.floor(Math.random() * (action.length - 1) + 1)];
+  let whatRandom = what[Math.floor(Math.random() * (what.length - 1) + 1)];
+  let whenRandom = when[Math.floor(Math.random() * (when.length - 1) + 1)];
+  return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("excuse").innerHTML = onload();
